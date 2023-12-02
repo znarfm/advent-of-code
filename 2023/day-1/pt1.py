fd266e642b44997967e0a38afac0356f7451cee2
znarfm <44516102+znarfm@users.noreply.github.com>
2023-12-02 11:39:01 +0800

    day 1 pt1

---
+++ 2023/day-1/pt1.py
@@ -0,0 +1,10 @@
+# Day 1: Trebuchet?!
+digits = [str(x) for x in range(10)]
+sum = 0
+with open("calibration_vals.txt", "r") as f:
+# with open("day1_small.txt", "r") as f:
+    for line in f.readlines():
+        num_from_input = [char for char in line if char in digits]
+        # print(num_from_input)
+        sum += int(num_from_input[0] + num_from_input[-1])
+print(sum)
