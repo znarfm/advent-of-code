920e6c27c547092c93e58b9480c3611dc4c31055
znarfm <44516102+znarfm@users.noreply.github.com>
2023-12-02 13:25:46 +0800

    Minor changes

--- 2023/day-1/pt1.py
+++ 2023/day-1/pt1.py
@@ -1,8 +1,8 @@
-# Day 1: Trebuchet?!
+# Day 1: Trebuchet?! (pt. 1)
 digits = [str(x) for x in range(10)]
 sum = 0
 with open("calibration_vals.txt", "r") as f:
-# with open("day1_small.txt", "r") as f:
+# with open("pt1_example.txt", "r") as f:
     for line in f.readlines():
         num_from_input = [char for char in line if char in digits]
         # print(num_from_input)
